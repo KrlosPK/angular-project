803a110f9fc497ce6040f9667ef4fdf82590817c
KrlosPK <centille095@gmail.com>
2022-10-28 08:43:31 -0500

    Proyecto creado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { UsuariosComponent } from './components/usuarios/usuarios.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { ServiciosComponent } from './components/servicios/servicios.component';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'inicio' },
+  { path: 'inicio', component: InicioComponent },
+  { path: 'usuarios', component: UsuariosComponent },
+  { path: 'servicios', component: ServiciosComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsuariosComponent } from './components/usuarios/usuarios.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { ServiciosComponent } from './components/servicios/servicios.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsuariosComponent,
+    InicioComponent,
+    ServiciosComponent
   ],
   imports: [
     BrowserModule,
